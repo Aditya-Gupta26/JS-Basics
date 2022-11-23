3c902efe768ac1b359d21ee9c49d0592e83b41a6
Aditya Gupta <adigupta2602@gmail.com>
2022-11-23 06:58:02 +0000

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,18 @@
+
+var a = 5;
+const day = "s";
+console.log(day)
+var sak = "sak"
+let numbeer = 17
+console.log(typeof day)
+const names = ["a","b","cdefg","bara","goeyalll"];
+console.log(names.map(name => name.length == 5));
+
+try {
+    const a = "jgh";
+    a = "abd"
+}
+catch(e){
+    
+    console.log(e)
+}
